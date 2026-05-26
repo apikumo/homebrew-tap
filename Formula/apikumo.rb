76fb1a001aa1242f42253b30ade604f900b75d1a
meandp589 <mean.dp589@gmail.com>
2026-05-26 20:05:16 +0700

    chore: point at apikumo org URLs after transfer

--- Formula/apikumo.rb
+++ Formula/apikumo.rb
@@ -1,7 +1,7 @@
 class Apikumo < Formula
   desc "Sync your OpenAPI spec with apikumo from the command line"
   homepage "https://apikumo.com"
-  url "https://github.com/meandp589/apikumo-releases/releases/download/v0.1.0/apikumo-0.1.0.tgz"
+  url "https://github.com/apikumo/releases/releases/download/v0.1.0/apikumo-0.1.0.tgz"
   sha256 "81df9fb6a8769c5be4a8e420999903c9be44f5984028c4df4744293435812fa0"
   license "MIT"
 
